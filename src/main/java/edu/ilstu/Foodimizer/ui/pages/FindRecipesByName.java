ce9bed17ddc82a2efad923d2c3a1f3c1efc8f9e7
snahuy <sna.huy0904@gmail.com>
2022-10-26 22:40:20 -0500

    Initial Commit for SearchPage

--- src/main/java/edu/ilstu/Foodimizer/ui/pages/FindRecipesByName.java
+++ src/main/java/edu/ilstu/Foodimizer/ui/pages/FindRecipesByName.java
@@ -1,34 +1,62 @@
 package edu.ilstu.Foodimizer.ui.pages;
 
-import edu.ilstu.Foodimizer.app.db.models.Recipe;
-import edu.ilstu.Foodimizer.app.db.service.RecipeService;
+import edu.ilstu.Foodimizer.Main;
 import edu.ilstu.Foodimizer.ui.MainWindowContentManager;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 
-import java.util.List;
-
-public class FindRecipesByName extends Page {
+public class FindRecipesByName extends JPanel {
     public FindRecipesByName() {
         init();
     }
 
     private void init() {
-        List<Recipe> recipes = new RecipeService().getAll();
-        System.out.println(recipes);
-        for (Recipe recipe : recipes) {
-            JButton testbutton = new JButton(recipe.getName());
-            testbutton.addActionListener(e->buttonPressed(recipe));
-            add(testbutton);
-        }
+        setBackground(Color.white);
+        testButton = new JButton("Recipe");
+        testButton.addActionListener(e -> buttonPressed());
+
+        // Changes
+
+        title = new JLabel("Foodimizer");
+        title.setFont(new Font("Segoe UI",1, 24)); // Set the font of the title
+        title.setBounds(130,100,100,40);
+
+        searchPrompt = new JLabel("Search Recipe:");
+        searchPrompt.setFont(new Font("Segoe UI",1, 14));
+
+        searchText = new JTextField();
+        searchText.setBounds(100,130,480,30);
+        searchText.setColumns(20);
+        searchText.setBorder(new EmptyBorder(10,5,10,5));
+        searchText.setVisible(true);
+
+        searchButton = new JButton("Search");
+        searchButton.setBounds(500,130,80,30);
+        searchButton.setFont(searchText.getFont());
+        searchButton.setBorder(new EmptyBorder(10,5,19,5));
+
+        add(title);
+        add(searchPrompt);
+        add(searchText);
+
+
     }
 
-    private void buttonPressed(Recipe r) {
+    private void buttonPressed() {
         contentManager = MainWindowContentManager.getInstance();
-        RecipePage.getInstance().setActiveRecipe(r);
         contentManager.goToPage("RecipePage");
     }
 
     private MainWindowContentManager contentManager;
     private RecipePage recipePage;
+    private JButton testButton;
+
+    // Changes
+    private JButton searchButton;
+
+    private JLabel title;
+    private JLabel searchPrompt;
+    private JTextField searchText;
 }
