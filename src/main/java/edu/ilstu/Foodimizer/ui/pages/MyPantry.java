95cf1b3ae8da37484df9f70da977648064d869c0
Redakted <40189344+iRedakted@users.noreply.github.com>
2022-10-29 16:19:04 -0500

    Created persistPantry method.

--- src/main/java/edu/ilstu/Foodimizer/ui/pages/MyPantry.java
+++ src/main/java/edu/ilstu/Foodimizer/ui/pages/MyPantry.java
@@ -1,14 +1,25 @@
 package edu.ilstu.Foodimizer.ui.pages;
 
+import edu.ilstu.Foodimizer.app.db.models.Ingredient;
+import edu.ilstu.Foodimizer.app.db.models.Profile;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class MyPantry extends JPanel {
     public MyPantry() {
         init();
+        persistPantry();
     }
 
     private void init() {
         setBackground(Color.RED);
+    }
+    private void persistPantry(){
+        Ingredient ingredient = new Ingredient();
+        Profile profile = new Profile();
+
+        ingredient.getName();
+        profile.getProfileId();
     }
 }
