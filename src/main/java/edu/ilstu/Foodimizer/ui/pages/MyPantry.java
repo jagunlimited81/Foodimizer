a2ce696567a2c25584f4c34cdc60ded7d0efc6a7
Redakted <40189344+iRedakted@users.noreply.github.com>
2022-11-30 22:24:12 -0600

    Created Panels and Buttons for pantry addition & removal.

--- src/main/java/edu/ilstu/Foodimizer/ui/pages/MyPantry.java
+++ src/main/java/edu/ilstu/Foodimizer/ui/pages/MyPantry.java
@@ -23,7 +23,7 @@ public class MyPantry extends JPanel {
     JList<Ingredient> list = new JList<>();
     DefaultListModel<Ingredient> model = new DefaultListModel<>();
     JLabel label = new JLabel();
-    JSplitPane splitPane = new JSplitPane();
+//    JSplitPane splitPane = new JSplitPane();
     IngredientService is = new IngredientService();
     JButton fireButton;
 
@@ -34,9 +34,12 @@ public class MyPantry extends JPanel {
     }
 
     private void init() {
-        JButton jb = new JButton("Add");
-        jb.addActionListener(e ->persistPantry());
-        this.add(jb);
+        JButton addBtn = new JButton("Add");
+        addBtn.addActionListener(e ->persistPantry());
+        this.add(addBtn);
+        JButton rmvBtn = new JButton("Remove");
+        rmvBtn.addActionListener(e ->persistPantry());
+        this.add(rmvBtn);
 //        setBackground(Color.WHITE);
         contentPane = new JPanel();
         this.setLayout(new BorderLayout());
@@ -44,8 +47,10 @@ public class MyPantry extends JPanel {
 
 
         /* Pantry Items Panel */
-        contentPane.setBackground(Color.BLACK);
-        this.add(contentPane, BorderLayout.CENTER);
+        contentPane.setBackground(Color.PINK);
+//        this.add(contentPane, BorderLayout.CENTER);
+        contentPane.add(addBtn);
+        contentPane.add(rmvBtn);
 
         /* Pantry Search Bar */
         panel = new JPanel();
@@ -55,9 +60,12 @@ public class MyPantry extends JPanel {
         activePantry.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
         activePantry.setLayoutOrientation(JList.HORIZONTAL_WRAP);
         activePantry.setVisibleRowCount(-1);
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,activePantry,contentPane);
 //        JScrollPane listScroller = new JScrollPane(activePantry);
 //        listScroller.setPreferredSize(new Dimension(getPreferredSize()));
-        this.add(panel, BorderLayout.NORTH);
+        this.add(panel, BorderLayout.EAST);
+        splitPane.setLeftComponent(new JScrollPane(activePantry));
+        splitPane.setRightComponent(contentPane);
         this.add(splitPane);
     }
 
