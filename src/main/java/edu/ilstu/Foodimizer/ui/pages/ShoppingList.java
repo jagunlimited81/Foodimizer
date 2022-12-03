394b8e4b68c273d7375b328167b46d18b0247905
Levy, Nick <nrlevy@ilstu.edu>
2022-12-03 12:55:32 -0600

    - Implemented GroupLayout rather than BorderLayout for the page - AddIngredient to Shopping List Functionality is now fully operational - Clearlist Functionality is almost fully operational     - Bug where the page doesn't fully refresh until leaving the page and coming back

--- src/main/java/edu/ilstu/Foodimizer/ui/pages/ShoppingList.java
+++ src/main/java/edu/ilstu/Foodimizer/ui/pages/ShoppingList.java
@@ -1,86 +1,80 @@
 package edu.ilstu.Foodimizer.ui.pages;
 
-import edu.ilstu.Foodimizer.app.db.models.Ingredient;
-import edu.ilstu.Foodimizer.app.db.service.IngredientService;
 import edu.ilstu.Foodimizer.app.StateManager;
+import edu.ilstu.Foodimizer.app.db.models.Ingredient;
 import edu.ilstu.Foodimizer.app.db.models.Profile;
-
+import edu.ilstu.Foodimizer.app.db.service.IngredientService;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
-
 public class ShoppingList extends Page {
+
     public ShoppingList() {
+        super();
         init();
-
     }
-    public void init() {
-        Profile activeProfile = StateManager.getInstance().getActiveProfile();
-        if(activeProfile == null)
-            return;
 
-        // Create 2 Panels for Shopping List and Buttons to Clear and Save to PDF
-        JPanel contentPanel = new JPanel(new BorderLayout());
-        JPanel shopListPanel = new JPanel(new BorderLayout());
-        JPanel buttonPanel = new JPanel(new BorderLayout());
-        JPanel addIngredientPanel = new JPanel(new BorderLayout());
+    @Override
+    protected void init() { //TODO implement everyhing else
+        if (StateManager.getInstance().getActiveProfile() == null)
+            return;
+        contentPanel = new JPanel();
+        GroupLayout layout = new GroupLayout(contentPanel);
+        contentPanel.setLayout(layout);
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+
+        ArrayList<Ingredient> ingredients = new ArrayList<>(StateManager.getInstance().getActiveProfile().getShoppingList());
+        JList<Object> shoppingList = new JList<>( ingredients.toArray());
+
+        JLabel title = new JLabel("My Shopping List");
+        title.setFont(new Font("Verdana", Font.PLAIN, 22));
 
         //Create Buttons to be able to trigger the clearing of the list, and saving the List to PDF
         JButton removeIngButton = new JButton("Remove Ingredient from the Shopping List");
         JButton clearListButton = new JButton("Clear the Shopping List");
         clearListButton.addActionListener(e->clearShoppingList());
-
-        //Button for printing the Shopping List to PDF
         JButton pdfButton = new JButton("Print Shopping List to PDF");
-        //pdfButton.addActionListener(e->saveToPDF());
-
-        // Panel for storing the add ingredient button
-        // create a label to display text
-        // create the add button
+//        pdfButton.addActionListener(e->saveToPDF());
         JButton addIngButton = new JButton("Add Ingredient");
-        JTextField ingText = new JTextField();
-        addIngButton.addActionListener(e->addIngredientToShoppingList(ingText));
-
-        JLabel ingSearchLabel = new JLabel("Add Ingredient to Shopping List: ");
-        addIngredientPanel.add(ingText,BorderLayout.PAGE_START);
-        //addIngredientPanel.add(ingSearchLabel,BorderLayout.PAGE_START);
-        addIngredientPanel.add(addIngButton);
-        addIngredientPanel.setPreferredSize(new Dimension(250,500));
+        JTextField addIngText = new JTextField();
+        addIngButton.addActionListener(e->addIngredientToShoppingList(addIngText));
 
 
-//      Create a JList to display the array
-//      Import ingredients over into the list;
-        groceryList = new JList(activeProfile.getShoppingList().toArray());
-        groceryList.setBorder(BorderFactory.createTitledBorder("Foodimizer Grocery List"));
-        groceryList.setPreferredSize(new Dimension(500,500));
+        layout.setHorizontalGroup(
+                layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER)
+                                .addComponent(shoppingList)
+                                .addComponent(title))
+                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                .addComponent(addIngText)
+                                .addComponent(addIngButton)
+                                .addComponent(removeIngButton)
+                                .addComponent(clearListButton)
+                                .addComponent(pdfButton))
 
-//      //Add the Shopping list Panel to side of the content Panel
-        shopListPanel.add(groceryList);
+        );
+        layout.setVerticalGroup(
+                layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER)
+                                .addComponent(shoppingList)
+                                .addComponent(title))
+                        .addComponent(addIngText)
+                        .addComponent(addIngButton)
+                        .addComponent(removeIngButton)
+                        .addComponent(clearListButton)
+                        .addComponent(pdfButton)
 
-//         Add the buttons to the right side panel
-//        buttonPanel.add(addIngredientPanel);
-        buttonPanel.add(removeIngButton);
-        buttonPanel.add(clearListButton, BorderLayout.PAGE_START);
-        buttonPanel.add(pdfButton,BorderLayout.PAGE_END);
-
-        contentPanel.add(shopListPanel,BorderLayout.WEST);
-        contentPanel.add(addIngredientPanel);
-        contentPanel.add(buttonPanel,BorderLayout.SOUTH);
-        add(contentPanel);
+        );
+        this.add(contentPanel);
 
     }
 
-    //List of ingredients imported to the shopping list from other pages
-    private JList groceryList;
-
-    private void removeIngredientFromShoppingList(Ingredient ingToRemove)
-    {
-        Profile activeProfile = StateManager.getInstance().getActiveProfile();
-        activeProfile.getShoppingList().remove(ingToRemove);
-        refreshContent();
-    }
+    //JPanels
+    JPanel contentPanel;
 
     private void clearShoppingList()
     {
@@ -88,6 +82,7 @@ public class ShoppingList extends Page {
         activeProfile.getShoppingList().clear();
         refreshContent();
     }
+
 
     private void addIngredientToShoppingList(JTextField ingText)
     {
@@ -110,6 +105,5 @@ public class ShoppingList extends Page {
         refreshContent();
 
     }
-
 
 }
