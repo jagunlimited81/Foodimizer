c41230e3d03b57474a0ebf547affc65af24c9244
jagunlimited81 <jagunlimited81@gmail.com>
2022-12-06 06:02:47 -0500

    Bugfix for Earlier commit (#57)

--- src/main/java/edu/ilstu/Foodimizer/app/DatabaseFiller.java
+++ src/main/java/edu/ilstu/Foodimizer/app/DatabaseFiller.java
@@ -78,9 +78,10 @@ public class DatabaseFiller {
         recipe.setCookMethod("Stove");
         recipe.setServingSize(1);
         recipe.setDescription("A warm english meal");
-        recipe.setDirections("1. Fold egg in half" +
-                "\n2. veat egg" +
-                "\n3. cook the cheese on the muffin");
+        recipe.setDirections("""
+                1. Fold egg in half
+                2. veat egg
+                3. cook the cheese on the muffin""");
         recipe.setMealType("Breakfast");
         recipe.setPrepTime(0);
         recipe.setCookTime(10);
@@ -91,10 +92,11 @@ public class DatabaseFiller {
         ingredients = new String[]{"bread", "peanut butter", "grape jelly", "strawberry jelly"};
         recipe = createRecipeFromString(recipeName, ingredients);
         recipe.setDescription("A staple of American childhood foods, peanut butter and jelly sandwich or PBJ");
-        recipe.setDirections("1. Spread the peanut butter on one piece of bread." +
-                "\n2. Spread the jelly on the other side." +
-                "\n3. Put the two pieces of bread together to form a sandwich." +
-                "\n4. Toddler adaptation: cut off crusts before serving.");
+        recipe.setDirections("""
+                1. Spread the peanut butter on one piece of bread.
+                2. Spread the jelly on the other side.
+                3. Put the two pieces of bread together to form a sandwich.
+                4. Toddler adaptation: cut off crusts before serving.""");
         recipe.setMealType("Snack");
         recipe.setPrepTime(0);
         recipe.setCookTime(5);
@@ -107,11 +109,12 @@ public class DatabaseFiller {
         recipe.setCookMethod("Oven");
         recipe.setServingSize(1);
         recipe.setDescription("Between homemade, restaurant, and store-bought, these are the best black bean burgers you will ever made.");
-        recipe.setDirections("1. Preheat oven to 325°F (163°C). " +
-                "\n2. Spread beans evenly onto a lined baking sheet and bake for 15 minutes." +
-                "\n3. Meanwhile, sauté olive oil, chopped pepper, onion, and garlic over medium heat until peppers and onions are soft, about 5-6 minutes." +
-                "\n4. Form into patties– about 1/3 cup of mixture in each." +
-                "\n5. Place patties on a parchment paper lined baking sheet and bake at 375°F (191°C) for 10 minutes on each side, 20 minutes total");
+        recipe.setDirections("""
+                1. Preheat oven to 325°F (163°C).\s
+                2. Spread beans evenly onto a lined baking sheet and bake for 15 minutes.
+                3. Meanwhile, sauté olive oil, chopped pepper, onion, and garlic over medium heat until peppers and onions are soft, about 5-6 minutes.
+                4. Form into patties– about 1/3 cup of mixture in each.
+                5. Place patties on a parchment paper lined baking sheet and bake at 375°F (191°C) for 10 minutes on each side, 20 minutes total""");
         recipe.setMealType("Lunch and Dinner");
         recipe.setPrepTime(10);
         recipe.setCookTime(15);
@@ -125,12 +128,13 @@ public class DatabaseFiller {
         recipe.setCookMethod("Pan");
         recipe.setServingSize(1);
         recipe.setDescription("Who wouldn't love to make their own classic hamburger at home?");
-        recipe.setDirections("1. In a bowl, mix ground beef, egg, onion, bread crumbs, Worcestershire, garlic, 1/2 teaspoon salt, and 1/4 teaspoon pepper until well blended. " +
-                "\n2. Divide mixture into four equal portions and shape each into a patty about 4 inches wide." +
-                "\n3. Lay burgers on an oiled barbecue grill over a solid bed of hot coals or high heat on a gas grill (you can hold your hand at grill level only 2 to 3 seconds); close lid on gas grill. " +
-                "\n4. Cook burgers, turning once, until browned on both sides and no longer pink inside (cut to test), 7 to 8 minutes total. Remove from grill." +
-                "\n5. Lay buns, cut side down, on grill and cook until lightly toasted, 30 seconds to 1 minute." +
-                "\n6. Spread mayonnaise and ketchup on bun bottoms. Add lettuce, tomato, burger, onion, and salt and pepper to taste. Set bun tops in place");
+        recipe.setDirections("""
+                1. In a bowl, mix ground beef, egg, onion, bread crumbs, Worcestershire, garlic, 1/2 teaspoon salt, and 1/4 teaspoon pepper until well blended.\s
+                2. Divide mixture into four equal portions and shape each into a patty about 4 inches wide.
+                3. Lay burgers on an oiled barbecue grill over a solid bed of hot coals or high heat on a gas grill (you can hold your hand at grill level only 2 to 3 seconds); close lid on gas grill.\s
+                4. Cook burgers, turning once, until browned on both sides and no longer pink inside (cut to test), 7 to 8 minutes total. Remove from grill.
+                5. Lay buns, cut side down, on grill and cook until lightly toasted, 30 seconds to 1 minute.
+                6. Spread mayonnaise and ketchup on bun bottoms. Add lettuce, tomato, burger, onion, and salt and pepper to taste. Set bun tops in place""");
         recipe.setPrepTime(10);
         recipe.setCookTime(10);
         recipe.setWaitTime(10);
@@ -142,33 +146,19 @@ public class DatabaseFiller {
         recipe.setCookMethod("Pan");
         recipe.setServingSize(1);
         recipe.setDescription("A homemade chicken sandwich tastes fresher because you aren’t re-using the same oil all day long");
-        recipe.setDirections("1.Pound chicken cutlets between plastic wrap to an even 1/2” thickness for even frying." +
-                "\n2.Marinate chicken – In a large mixing bowl, combine marinade ingredients, add chicken and stir to coat. " +
-                "\n3.Cover and refrigerate 2-4 hours or overnight." +
-                "\n4.Dredge Chicken – In a separate shallow bowl, whisk together: flour, baking powder, salt, pepper, paprika, onion powder, and garlic powder. " +
-                "\n5.Lift a piece of marinated chicken, letting excess marinade drip off, and dip chicken into the flour mixture, pressing it on with your hands to coat well. " +
-                "\n6.Transfer to a wire rack. Keep the flour mixture for step 5." +
-                "\n7.Heat 1-inch of oil in a soup pot or dutch oven to 350˚F. See our favorite oils for frying below." +
-                "\n8.Re-dip the chicken into the flour mixture just before frying. Add chicken 2 pieces at a time so you don’t crowd.");
+        recipe.setDirections("""
+                1.Pound chicken cutlets between plastic wrap to an even 1/2” thickness for even frying.
+                2.Marinate chicken – In a large mixing bowl, combine marinade ingredients, add chicken and stir to coat.\s
+                3.Cover and refrigerate 2-4 hours or overnight.
+                4.Dredge Chicken – In a separate shallow bowl, whisk together: flour, baking powder, salt, pepper, paprika, onion powder, and garlic powder.\s
+                5.Lift a piece of marinated chicken, letting excess marinade drip off, and dip chicken into the flour mixture, pressing it on with your hands to coat well.\s
+                6.Transfer to a wire rack. Keep the flour mixture for step 5.
+                7.Heat 1-inch of oil in a soup pot or dutch oven to 350˚F. See our favorite oils for frying below.
+                8.Re-dip the chicken into the flour mixture just before frying. Add chicken 2 pieces at a time so you don’t crowd.""");
         recipe.setMealType("Lunch and Dinner");
         recipe.setPrepTime(4);
         recipe.setCookTime(10);
         recipe.setWaitTime(10);
-        recipeService.save(recipe);
-
-        recipeName = "Fries Burger";
-        ingredients = new String[]{"hamburger bun", "fries", "cheese", "lettuce", "oil", "tomato", "pickle", "mayonnaise"};
-        recipe = createRecipeFromString(recipeName, ingredients);
-        recipe.setCookMethod("Pan");
-        recipe.setServingSize(1);
-        recipe.setDescription("Just a hamburger bun with fries can make you thirsty.");
-        recipe.setDirections("1. Put your hamburger ban in your toaster" +
-                "\n2. Heat your frying pan then add your oil to the pan. Throw your fries into the frying pan." +
-                "\n3. After the fries are cooked, add them to your hamburger bun with and top it off with the melted cheese");
-        recipe.setMealType("Lunch and Dinner");
-        recipe.setPrepTime(0);
-        recipe.setCookTime(5);
-        recipe.setWaitTime(5);
         recipeService.save(recipe);
 
         recipeName = "Horseshoe Burger";
@@ -176,19 +166,21 @@ public class DatabaseFiller {
         recipe = createRecipeFromString(recipeName, ingredients);
         recipe.setCookMethod("Oven and Skillet");
         recipe.setServingSize(1);
-        recipe.setDescription("Every time we visit Springfield, Illinois we have to eat one of their famous horseshoes. " +
-                "\nThe cheese sauce is a little different depending on where you stop to eat, " +
-                "\nseems everyone has their own way of making it.");
-        recipe.setDirections("1. Preheat the oven to 400 degrees F (200 degrees C). " +
-                "\n2. Spread french fries out on a baking sheet. Bake for 20 minutes in the preheated oven, or until golden brown. " +
-                "\n3. Meanwhile, divide the ground beef into four equal parts and form into patties. " +
-                "\n4. Fry the patties in a large skillet over medium-high heat until well done, about 4 minutes per side. " +
-                "\n5. Melt the butter in a saucepan over medium heat and whisk in the flour. " +
-                "\n6. Cook while stirring constantly for 4 minutes to cook out the flavor of raw flour." +
-                "\n7. Gradually whisk in the half-and-half so that no lumps form. " +
-                "\n8. Bring to a simmer then remove from the heat and add the cheese; stir until melted. Season with salt, pepper, and hot sauce to taste. " +
-                "\n9. Place 2 slices of toasted bread onto 2 dinner plates and top each slice with a hamburger patty." +
-                "\n10. Top each patty with cooked french fries. Pour the cheese sauce on top of everything and serve immediately. ");
+        recipe.setDescription("""
+                Every time we visit Springfield, Illinois we have to eat one of their famous horseshoes.\s
+                The cheese sauce is a little different depending on where you stop to eat,\s
+                seems everyone has their own way of making it.""");
+        recipe.setDirections("""
+                1. Preheat the oven to 400 degrees F (200 degrees C).\s
+                2. Spread french fries out on a baking sheet. Bake for 20 minutes in the preheated oven, or until golden brown.\s
+                3. Meanwhile, divide the ground beef into four equal parts and form into patties.\s
+                4. Fry the patties in a large skillet over medium-high heat until well done, about 4 minutes per side.\s
+                5. Melt the butter in a saucepan over medium heat and whisk in the flour.\s
+                6. Cook while stirring constantly for 4 minutes to cook out the flavor of raw flour.
+                7. Gradually whisk in the half-and-half so that no lumps form.\s
+                8. Bring to a simmer then remove from the heat and add the cheese; stir until melted. Season with salt, pepper, and hot sauce to taste.\s
+                9. Place 2 slices of toasted bread onto 2 dinner plates and top each slice with a hamburger patty.
+                10. Top each patty with cooked french fries. Pour the cheese sauce on top of everything and serve immediately.\s""");
         recipe.setMealType("Lunch and Dinner");
         recipe.setPrepTime(0);
         recipe.setCookTime(24);
@@ -199,55 +191,6 @@ public class DatabaseFiller {
         ingredients = new String[]{"bacon", "ground beef", "hamburger bun", "cheese", "lettuce", "tomato", "salt", "pepper", "pickles", "ketchup"};
         recipe = createRecipeFromString(recipeName, ingredients);
         recipe.setDescription("The ultimate bacon cheeseburger with beef cooked in bacon fat, a bacon fat mayonnaise, onions caramelized in bacon fat, buns toasted in bacon fat, and a crisp bacon weave topping. ");
-        recipeService.save(recipe);
-
-        recipeName = "Black Beans Burger";
-        ingredients = new String[]{"black beans", "hamburger bun", "olive oil", "bell pepper", "yellow onion", "garlic cloves", "ground cumin", "chilli powder", "garlic powder", "paprika", "bread crumbs", "feta cheese", "egg", "salt", "pepper", "ketchup"};
-        recipe = createRecipeFromString(recipeName, ingredients);
-        recipe.setCookMethod("Oven");
-        recipe.setServingSize(1);
-        recipe.setDescription("Between homemade, restaurant, and store-bought, these are the best black bean burgers you will ever made.");
-//        recipe.setDirections("1. Preheat oven to 325°F (163°C). Spread beans evenly onto a lined baking sheet and bake for 15 minutes." +
-//                "\n2. Meanwhile, sauté olive oil, chopped pepper, onion, and garlic over medium heat until peppers and onions are soft, about 5-6 minutes." +
-//                "\n3. Form into patties– about 1/3 cup of mixture in each." +
-//                "\n4. Place patties on a parchment paper lined baking sheet and bake at 375°F (191°C) for 10 minutes on each side, 20 minutes total");
-        recipe.setMealType("Lunch and Dinner");
-        recipe.setPrepTime(10);
-        recipe.setCookTime(15);
-        recipe.setWaitTime(20);
-        recipeService.save(recipe);
-
-
-        recipeName = "Classic Burger";
-        ingredients = new String[]{"beef", "hamburger bun", "egg", "onion", "dried bread crumbs", "worcestershire", "garlic", "salt", "pepper", "mayonnaise", "ketchup", "lettuce", "tomato", "red onion"};
-        recipe = createRecipeFromString(recipeName, ingredients);
-        recipe.setCookMethod("Pan");
-        recipe.setServingSize(1);
-        recipe.setDescription("Who wouldn't love to make their own classic hamburger at home?");
-//        recipe.setDirections("1. In a bowl, mix ground beef, egg, onion, bread crumbs, Worcestershire, garlic, 1/2 teaspoon salt, and 1/4 teaspoon pepper until well blended. Divide mixture into four equal portions and shape each into a patty about 4 inches wide." +
-//                        "\n2. Lay burgers on an oiled barbecue grill over a solid bed of hot coals or high heat on a gas grill (you can hold your hand at grill level only 2 to 3 seconds); close lid on gas grill. Cook burgers, turning once, until browned on both sides and no longer pink inside (cut to test), 7 to 8 minutes total. Remove from grill." +
-//                        "\n3. Lay buns, cut side down, on grill and cook until lightly toasted, 30 seconds to 1 minute." +
-//                        "\n4. Spread mayonnaise and ketchup on bun bottoms. Add lettuce, tomato, burger, onion, and salt and pepper to taste. Set bun tops in place");
-        recipe.setPrepTime(10);
-        recipe.setCookTime(10);
-        recipe.setWaitTime(10);
-        recipeService.save(recipe);
-
-        recipeName = "Chicken Sandwich";
-        ingredients = new String[]{"hamburger bun", "pickles", "lettuce", "tomato", "onion", "cheese", "mayonnaise"};
-        recipe = createRecipeFromString(recipeName, ingredients);
-        recipe.setCookMethod("Pan");
-        recipe.setServingSize(1);
-        recipe.setDescription("A homemade chicken sandwich tastes fresher because you aren’t re-using the same oil all day long");
-//        recipe.setDirections("1. Pound chicken cutlets between plastic wrap to an even 1/2” thickness for even frying." +
-//                "\n2. Marinate chicken – In a large mixing bowl, combine marinade ingredients, add chicken and stir to coat. Cover and refrigerate 2-4 hours or overnight." +
-//                "\n3. Dredge Chicken – In a separate shallow bowl, whisk together: flour, baking powder, salt, pepper, paprika, onion powder, and garlic powder. Lift a piece of marinated chicken, letting excess marinade drip off, and dip chicken into the flour mixture, pressing it on with your hands to coat well. Transfer to a wire rack. Keep the flour mixture for step 5." +
-//                "\n4. Heat 1-inch of oil in a soup pot or dutch oven to 350˚F. See our favorite oils for frying below." +
-//                "\n5. Re-dip the chicken into the flour mixture just before frying. Add chicken 2 pieces at a time so you don’t crowd.");
-        recipe.setMealType("Lunch and Dinner");
-        recipe.setPrepTime(4);
-        recipe.setCookTime(10);
-        recipe.setWaitTime(10);
         recipeService.save(recipe);
 
         recipeName = "Fries Burger";
@@ -264,23 +207,6 @@ public class DatabaseFiller {
         recipe.setPrepTime(0);
         recipe.setCookTime(5);
         recipe.setWaitTime(5);
-        recipeService.save(recipe);
-
-        recipeName = "Horseshoe Burger";
-        ingredients = new String[]{"fries", "ground beef", "bread", "butter", "flour", "shredded cheese", "salt", "ground pepper", "hot sauce"};
-        recipe = createRecipeFromString(recipeName, ingredients);
-        recipe.setCookMethod("Oven and Skillet");
-        recipe.setServingSize(1);
-        recipe.setDescription("Every time we visit Springfield, Illinois we have to eat one of their famous horseshoes. The cheese sauce is a little different depending on where you stop to eat, seems everyone has their own way of making it.");
-        recipe.setDirections("""
-                1. Preheat the oven to 400 degrees F (200 degrees C). Spread french fries out on a baking sheet. Bake for 20 minutes in the preheated oven, or until golden brown.\s
-                2. Meanwhile, divide the ground beef into four equal parts and form into patties. Fry the patties in a large skillet over medium-high heat until well done, about 4 minutes per side.\s
-                3. Melt the butter in a saucepan over medium heat and whisk in the flour. Cook while stirring constantly for 4 minutes to cook out the flavor of raw flour. Gradually whisk in the half-and-half so that no lumps form. Bring to a simmer then remove from the heat and add the cheese; stir until melted. Season with salt, pepper, and hot sauce to taste.\s
-                4. Place 2 slices of toasted bread onto 2 dinner plates and top each slice with a hamburger patty. Top each patty with cooked french fries. Pour the cheese sauce on top of everything and serve immediately.\s""");
-        recipe.setMealType("Lunch and Dinner");
-        recipe.setPrepTime(0);
-        recipe.setCookTime(24);
-        recipe.setWaitTime(30);
         recipeService.save(recipe);
 
         recipeName = "Ham and Cheese";
